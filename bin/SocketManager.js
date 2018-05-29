8b4743c6419c9a7df2307d3055e3a7e97f3e9507
rushil32 <rushil32@gmail.com>
2018-05-29 16:38:15 -0400

    Package.json changes

--- bin/SocketManager.js
+++ bin/SocketManager.js
@@ -4,4 +4,8 @@ module.exports = (socket) => {
   socket.on('vote', (partyId, trackId) => {
     socket.broadcast.emit('vote', partyId, trackId);
   });
+
+  socket.on('add-track', (partyId) => {
+    socket.broadcast.emit('add-track', partyId);
+  });
 };
