0c137de57c5be5c79b5f5adf6a45154ec948fda8
rushil32 <rushil32@gmail.com>
2018-05-30 11:47:18 -0400

    Changed redirect URI for heroku link

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     clientId: '4450915fbb874a0bbb5699e076955afd',
     secret: 'ac4566482fd94815aff2af06035d8007',
     scope: 'user-modify-playback-state user-read-private user-read-email user-read-playback-state',
-    redirectUri: 'https://warm-tor-34894.herokuapp.com',
+    redirectUri: 'https://dj-rooomba.herokuapp.com',
     token: '',
     tokenRefreshTime: '',
   },
